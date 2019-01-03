52787c3f73cde18d964c3ee6618ba5e5b1fe9ae5
Jérôme DESFIEUX <jerome.desfieux@navblue.aero>
2019-01-03 13:44:47 +0100

    Feature sliding label

--- src/osgEarthDrivers/label_annotation/AnnotationLabelSource.cpp
+++ src/osgEarthDrivers/label_annotation/AnnotationLabelSource.cpp
@@ -18,6 +18,7 @@
  */
 #include <osgEarthFeatures/LabelSource>
 #include <osgEarthFeatures/FeatureSourceIndexNode>
+#include <osgEarthFeatures/GeometryUtils>
 #include <osgEarthFeatures/FilterContext>
 #include <osgEarthAnnotation/LabelNode>
 #include <osgEarthAnnotation/PlaceNode>
@@ -64,6 +65,7 @@ public:
         NumericExpression textSizeExpr    ( text ? *text->size()     : NumericExpression() );
         NumericExpression textRotationExpr( text ? *text->onScreenRotation() : NumericExpression() );
         NumericExpression textCourseExpr  ( text ? *text->geographicCourse() : NumericExpression() );
+        StringExpression  textOffsetSupportExpr ( text ? *text->autoOffsetGeomWKT()  : StringExpression() );
         StringExpression  iconUrlExpr     ( icon ? *icon->url()      : StringExpression() );
         NumericExpression iconScaleExpr   ( icon ? *icon->scale()    : NumericExpression() );
         NumericExpression iconHeadingExpr ( icon ? *icon->heading()  : NumericExpression() );
@@ -111,6 +113,9 @@ public:
 
                 if ( text->geographicCourse().isSet() )
                     tempStyle.get<TextSymbol>()->geographicCourse()->setLiteral( feature->eval(textCourseExpr, &context) );
+
+                if ( text->autoOffsetGeomWKT().isSet() )
+                    tempStyle.get<TextSymbol>()->autoOffsetGeomWKT()->setLiteral( feature->eval( textOffsetSupportExpr, &context ) );
             }
 
             if ( icon )
@@ -124,12 +129,14 @@ public:
                 if ( icon->heading().isSet() )
                     tempStyle.get<IconSymbol>()->heading()->setLiteral( feature->eval(iconHeadingExpr, &context) );
             }
-            
+
             osg::Node* node = makePlaceNode(
                 context,
                 feature,
                 tempStyle,
-                textPriorityExpr);
+                textPriorityExpr,
+                text &&
+                    ( text->autoOffsetAlongLine() == true || text->autoRotateAlongLine() == true || text->autoOffsetGeomWKT().isSet() ) );
 
             if ( node )
             {
@@ -149,25 +156,79 @@ public:
     osg::Node* makePlaceNode(FilterContext&     context,
                              Feature*           feature, 
                              const Style&       style, 
-                             NumericExpression& priorityExpr )
+                             NumericExpression& priorityExpr,
+                             bool autoLineFollowing = false)
     {
-        osg::Vec3d center = feature->getGeometry()->getBounds().center();
+        const osg::Vec3d center = feature->getGeometry()->getBounds().center();
+        GeoPoint geoCenter = buildGeoPoint(center, style.getSymbol<AltitudeSymbol>(), feature->getSRS());
+        PlaceNode* node = 0L;
 
-        AltitudeMode mode = ALTMODE_ABSOLUTE;        
-
-        const AltitudeSymbol* alt = style.getSymbol<AltitudeSymbol>();
-        if (alt &&
-           (alt->clamping() == AltitudeSymbol::CLAMP_TO_TERRAIN || alt->clamping() == AltitudeSymbol::CLAMP_RELATIVE_TO_TERRAIN) &&
-           alt->technique() == AltitudeSymbol::TECHNIQUE_SCENE)
+        if ( autoLineFollowing )
         {
-            mode = ALTMODE_RELATIVE;
-        }                              
+            const Geometry* geom = feature->getGeometry();
+            if( style.getSymbol<TextSymbol>()->autoOffsetGeomWKT().isSet() )
+        {
+                StringExpression autoOffset = *(style.getSymbol<TextSymbol>()->autoOffsetGeomWKT());
+                std::string lineSupport = feature->eval(autoOffset, &context);
+                if (! lineSupport.empty() )
+                    geom = osgEarth::Features::GeometryUtils::geometryFromWKT(lineSupport);
+        }
 
-        GeoPoint point(feature->getSRS(), center.x(), center.y(), center.z(), mode);        
+            const LineString* geomLineString = 0L;
+            if( geom && geom->getComponentType() == Geometry::TYPE_LINESTRING )
+        {
+                if( geom->getType() == Geometry::TYPE_LINESTRING)
+            {
+                    geomLineString = dynamic_cast<const LineString*>( geom );
+            }
+            else
+            {
+                    const MultiGeometry* geomMulti = dynamic_cast<const MultiGeometry*>(geom);
+                if( geomMulti )
+                    geomLineString = dynamic_cast<const LineString*>( geomMulti->getComponents().front().get() );
+            }
+            }
 
-        PlaceNode* node = new PlaceNode(0L, point, style, context.getDBOptions());
-        
-        if ( !priorityExpr.empty() )
+            if( geomLineString )
+            {
+                GeoPoint geoStart = buildGeoPoint(geomLineString->front(), style.getSymbol<AltitudeSymbol>(), feature->getSRS(), true);
+                GeoPoint geoEnd = buildGeoPoint(geomLineString->back(), style.getSymbol<AltitudeSymbol>(), feature->getSRS(), true);
+
+                if( style.getSymbol<TextSymbol>()->autoOffsetGeomWKT().isSet() )
+                {
+                    node = new PlaceNode(0L, geoCenter, style, context.getDBOptions());
+                    // Direction to the longest distance
+                    if( (geoStart.vec3d() - geoCenter.vec3d()).length2() > (geoEnd.vec3d() - geoCenter.vec3d()).length2() )
+                        node->setLineCoords(geoEnd, geoStart);
+                    else
+                node->setLineCoords(geoStart, geoEnd);
+            }
+                else if( style.getSymbol<TextSymbol>()->autoOffsetPreferedPosition().isSet()
+                        && style.getSymbol<TextSymbol>()->autoOffsetPreferedPosition() == TextSymbol::LEFT )
+                {
+                    node = new PlaceNode(0L, geoStart, style, context.getDBOptions());
+                    node->setLineCoords(geoStart, geoEnd);
+                }
+                else if( style.getSymbol<TextSymbol>()->autoOffsetPreferedPosition().isSet()
+                         && style.getSymbol<TextSymbol>()->autoOffsetPreferedPosition() == TextSymbol::RIGHT )
+                {
+                    node = new PlaceNode(0L, geoEnd, style, context.getDBOptions());
+                    node->setLineCoords(geoEnd, geoStart);
+                }
+                else
+                {
+                    node = new PlaceNode(0L, geoCenter, style, context.getDBOptions());
+                    node->setLineCoords(geoStart, geoEnd);
+                }
+            }
+        }
+
+        else
+        {
+            node = new PlaceNode(0L, geoCenter, style, context.getDBOptions());
+        }
+
+        if ( !priorityExpr.empty() && node != 0L )
         {
             float val = feature->eval(priorityExpr, &context);
             node->setPriority( val >= 0.0f ? val : FLT_MAX );
@@ -176,6 +237,19 @@ public:
         return node;
     }
 
+    const GeoPoint buildGeoPoint( const osg::Vec3d& point, const AltitudeSymbol* altSym, const SpatialReference* srs, bool forceAbsolute = false )
+    {
+        AltitudeMode mode = ALTMODE_ABSOLUTE;
+
+        if (! forceAbsolute && altSym &&
+           (altSym->clamping() == AltitudeSymbol::CLAMP_TO_TERRAIN || altSym->clamping() == AltitudeSymbol::CLAMP_RELATIVE_TO_TERRAIN) &&
+           altSym->technique() == AltitudeSymbol::TECHNIQUE_SCENE)
+        {
+            mode = ALTMODE_RELATIVE;
+        }
+
+        return GeoPoint( srs, point.x(), point.y(), point.z(), mode );
+    }
 };
 
 //------------------------------------------------------------------------
