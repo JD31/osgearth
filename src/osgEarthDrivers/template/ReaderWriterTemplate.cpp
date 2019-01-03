3ca4e207ec93a3e77c8789b37db201d545427bdd
Jérôme DESFIEUX <jerome.desfieux@navblue.aero>
2019-01-03 13:53:55 +0100

    OsgEarth merge (reformat ?)

--- src/osgEarthDrivers/template/ReaderWriterTemplate.cpp
+++ src/osgEarthDrivers/template/ReaderWriterTemplate.cpp
@@ -27,75 +27,114 @@ using namespace NL::Template;
 */
 class TemplateReaderWriter: public osgDB::ReaderWriter
 {
-    public:
-        TemplateReaderWriter()
+public:
+    TemplateReaderWriter()
+    {
+        supportsExtension("template","Template");
+    }
+
+    virtual const char* className() const { return "TemplateReaderWriter"; }
+
+    virtual bool acceptsExtension(const std::string& extension) const
+    {
+        return osgDB::equalCaseInsensitive(extension,"template");
+    }
+
+    virtual ReadResult readObject(const std::string& file_name, const osgDB::Options* options) const
+    {
+        return readNode( file_name, options );
+    }
+
+    virtual ReadResult readNode(const std::string& fileName, const osgDB::Options* options) const
+    {
+        std::string ext = osgDB::getFileExtension( fileName );
+        if ( !acceptsExtension( ext ) )
+            return ReadResult::FILE_NOT_HANDLED;
+
+        std::string realName = osgDB::getNameLessExtension( fileName );
+        if (!osgDB::fileExists(realName))
         {
-            supportsExtension("template","Template");
+            return ReadResult::FILE_NOT_FOUND;
         }
 
-        virtual const char* className() const { return "TemplateReaderWriter"; }
-
-        virtual bool acceptsExtension(const std::string& extension) const
+        std::string realExt = osgDB::getFileExtension( realName );
+        osg::ref_ptr< osgDB::ReaderWriter > driver = osgDB::Registry::instance()->getReaderWriterForExtension(realExt);
+        if (!driver.valid())
         {
-            return osgDB::equalCaseInsensitive(extension,"template");
+            return ReadResult::FILE_NOT_HANDLED;
         }
 
-        virtual ReadResult readObject(const std::string& file_name, const osgDB::Options* options) const
+        LoaderFile loader;
+        Template t(loader);
+        t.load( realName.c_str() );
+
+
+        // Populate a list of key value pairs from the options string.
+        if (options)
         {
-            return readNode( file_name, options );
-        }
+            char escapeChar = '"';
+            std::string key, value;
+            bool currentCharIsKey = true;
+            bool isComplexValue = false;
+            std::string optionString = options->getOptionString();
 
-        virtual ReadResult readNode(const std::string& fileName, const osgDB::Options* options) const
-        {         
-            std::string ext = osgDB::getFileExtension( fileName );
-            if ( !acceptsExtension( ext ) )
-                return ReadResult::FILE_NOT_HANDLED;
-
-            std::string realName = osgDB::getNameLessExtension( fileName );
-            if (!osgDB::fileExists(realName))
+            for(char& c : optionString)
             {
-                return ReadResult::FILE_NOT_FOUND;
-            }
-
-            std::string realExt = osgDB::getFileExtension( realName );
-            osg::ref_ptr< osgDB::ReaderWriter > driver = osgDB::Registry::instance()->getReaderWriterForExtension(realExt);
-            if (!driver.valid())
-            {
-                return ReadResult::FILE_NOT_HANDLED;
-            }
-
-            LoaderFile loader;
-            Template t(loader);            
-            t.load( realName.c_str() );
-            
-
-            // Populate a list of key value pairs from the options string.
-            if (options)
-            {
-                std::istringstream iss(options->getOptionString());
-                std::string opt;
-                while (iss >> opt) {
-                    opt = osgDB::convertToLowerCase(opt);
-                    std::size_t eqInd = opt.find("=");
-                    std::string key = opt.substr(0, eqInd);
-                    std::string value = opt.substr(eqInd + 1);
-                    t.set(key, value);
+                //new value on the next char
+                if(c == '=' && !isComplexValue)
+                {
+                    currentCharIsKey = false;
+                }
+                //Start or end of complex value on the next char
+                else if(c == escapeChar)
+                {
+                    //we set the complex value
+                    isComplexValue = !isComplexValue;
+                    if(!isComplexValue)
+                    {
+                        if(!key.empty() && !value.empty())
+                        {
+                            t.set(key, value);
+                            key = "";
+                            value = "";
+                        }
+                    }
+                }
+                //new key on the next char
+                else if(c == ' ' && !isComplexValue)
+                {
+                    currentCharIsKey = true;
+                    if(!key.empty() && !value.empty())
+                    {
+                        t.set(key, value);
+                        key = "";
+                        value = "";
+                    }
+                }
+                //normal character
+                else
+                {
+                    if(isComplexValue || !currentCharIsKey)
+                        value.push_back(c);
+                    else
+                        key.push_back(c);
                 }
             }
+        }
 
-            OutputString output;
-            t.render( output );
+        OutputString output;
+        t.render( output );
 
-            OE_DEBUG << "Processed template " << std::endl << output.buf.str() << std::endl;
+        OE_DEBUG << "Processed template " << std::endl << output.buf.str() << std::endl;
 
-            // Set the osgEarth URIContext so that relative paths will work.  We have to do this manually here
-            // since we are using the stream based readNode function and the Earth driver won't know 
-            // where the original earth file came frame.
-            osg::ref_ptr< osgDB::Options > opt = osgEarth::Registry::instance()->cloneOrCreateOptions(options);
-            osgEarth::URIContext( realName ).store( opt.get() );
+        // Set the osgEarth URIContext so that relative paths will work.  We have to do this manually here
+        // since we are using the stream based readNode function and the Earth driver won't know
+        // where the original earth file came frame.
+        osg::ref_ptr< osgDB::Options > opt = osgEarth::Registry::instance()->cloneOrCreateOptions(options);
+        osgEarth::URIContext( realName ).store( opt.get() );
 
-            return driver->readNode( output.buf, opt.get() );                       
-        }        
+        return driver->readNode( output.buf, opt.get() );
+    }
 };
 
 REGISTER_OSGPLUGIN(template, TemplateReaderWriter)
